8326d322031b44eb98d70f029d84a574eda55270
Amal Ambrose <amalambrose555@gmail.com>
2025-12-29 19:22:08 +0530

    navigation updated

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -6,7 +6,6 @@ const navigations = [
         icon: 'pi pi-server', 
         child: [{ name: 'Buttons', icon: 'pi pi-circle', to: 'buttons' }]
     },
-    { name: 'Reports', icon: 'pi pi-chart-bar', to: 'reports' },
     { name: 'Settings', icon: 'pi pi-cog', to: 'settings' },
 ];
 
